a183fa39892f64a2cbcf209f5d9fcc369db3031a
kavunshiva <kavunshiva@gmail.com>
2017-06-02 10:18:34 -0400

    make it work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-  // your code here!
+  const postList = new PostList()
+  $('#photo-list').html(postList.render()) // add ul's to photo list
+  addFormListener(postList)
 })
+
+const addFormListener = (postList) => {
+  $('#photo-form').submit((e) => {
+    e.preventDefault()
+    const url = $('input[name=image_url]').val()
+    const caption = $('input[name=caption]').val()
+    const newPost = new Post(url, caption)
+    postList.addPost(newPost)
+    $('#photo-list ul').append(newPost.render())
+  })
+}
+
+class PostList {
+  constructor(){
+    this.array = []
+  }
+
+  all(){
+    return this.array
+  }
+  addPost(post){
+    this.array.push(post.render())
+  }
+  render(){
+    return `<ul>${this.array}</ul>`
+  }
+}
+
+class Post {
+  constructor(url, caption){
+    this.url = url
+    this.caption = caption
+  }
+  render(){
+    return `<li><img src="${this.url}" width=350px><br/>${this.caption}<br/><br/></li>`
+  }
+}
